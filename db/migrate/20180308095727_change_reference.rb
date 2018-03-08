04345561d323db163fb0acc6962eb1f725529580
Alexander Bogdanov <alexbogdanov@Alexanders-MacBook-Pro.local>
2018-03-08 12:14:35 +0200

    new migration

--- db/migrate/20180308095727_change_reference.rb
+++ db/migrate/20180308095727_change_reference.rb
@@ -1,5 +1,4 @@
 class ChangeReference < ActiveRecord::Migration[5.1]
   def change
-    change_column :table_name, :column_name, :new_type
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307233949) do
+ActiveRecord::Schema.define(version: 20180308095727) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
