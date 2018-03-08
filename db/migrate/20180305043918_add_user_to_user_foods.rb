165fb6aad5c68a9ff230a271b973958ccdd7690e
Alexander Bogdanov <alexbogdanov@Alexanders-MacBook-Pro.local>
2018-03-08 12:35:22 +0200

    Changed migration references

--- db/migrate/20180305043918_add_user_to_user_foods.rb
+++ db/migrate/20180305043918_add_user_to_user_foods.rb
@@ -1,5 +1,5 @@
 class AddUserToUserFoods < ActiveRecord::Migration[5.1]
   def change
-    add_column :user_foods, :user, :reference
+    add_foreign_key :user_foods, :user, foreign_key: true
   end
 end
--- db/migrate/20180306022123_add_user_id_to_goal.rb
+++ db/migrate/20180306022123_add_user_id_to_goal.rb
@@ -1,5 +1,5 @@
 class AddUserIdToGoal < ActiveRecord::Migration[5.1]
   def change
-    add_reference :goals, :user_id, foreign_key: true
+    add_foreign_key :goals, :user_id, foreign_key: true
   end
 end
