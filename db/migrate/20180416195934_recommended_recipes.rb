7faec7eb31fa86ee666da8725e2fb374fa814a66
Alexander Bogdanov <alex.bogdanovv14@gmail.com>
2018-04-16 23:04:22 +0300

    Another try to fix recommended recipes for heroku

---
+++ db/migrate/20180416195934_recommended_recipes.rb
@@ -0,0 +1,15 @@
+class RecommendedRecipes < ActiveRecord::Migration[5.1]
+  def change
+    def change
+      create_table :recommended_recipes do |t|
+        t.string :meals_response
+        t.integer :total_calories
+        t.integer :total_proteins
+        t.integer :total_fat
+        t.integer :total_carbohydrates
+        t.date :day
+        t.references :user
+  
+        t.timestamps
+  end
+end
